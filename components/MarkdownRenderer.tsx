12a1ef9fcc7b14385793c1a8f38798b20b46bbd2
Claude Djoumessi <djoumessiclaude95@gmail.com>
2025-08-20 15:11:58 +0100

    fix: fixed issue where math expr duplicates

--- components/MarkdownRenderer.tsx
+++ components/MarkdownRenderer.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import rehypeRaw from "rehype-raw";
+import 'katex/dist/katex.css'
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
@@ -20,43 +21,12 @@ function normalizeMathMarkdown(input: string): string {
 export default function MarkdownRenderer({
   children,
 }: {
-  children: React.ReactNode;
+  children: string;
 }) {
   return (
     <Markdown
       remarkPlugins={[remarkGfm, remarkMath]}
       rehypePlugins={[rehypeRaw, rehypeKatex]}
-      components={{
-        code({ node, inline, className, children: codeChildren, ...props }) {
-          const match = /language-(\w+)/.exec(className || "");
-
-          return !inline && match ? (
-            <SyntaxHighlighter
-              style={dracula}
-              PreTag="div"
-              language={match[1]}
-              {...props}
-            >
-              {String(codeChildren).replace(/\n$/, "")}
-            </SyntaxHighlighter>
-          ) : (
-            <code className={className} {...props}>
-              {codeChildren}
-            </code>
-          );
-        },
-
-        p({ node, children, ...props }) {
-          return (
-            <p
-              style={{ marginBottom: "0.5rem", whiteSpace: "pre-line" }}
-              {...props}
-            >
-              {children}
-            </p>
-          );
-        },
-      }}
     >
       {normalizeMathMarkdown(children)}
     </Markdown>
